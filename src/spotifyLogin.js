102ca91aa5524d5846c9609184fdebb00c3afdac
gianlu <gianlucademaio0@gmail.com>
2025-04-11 20:07:24 +0200

    .

--- src/spotifyLogin.js
+++ src/spotifyLogin.js
@@ -1,7 +1,7 @@
 export const handleLogin = () => {
     
     const clientId = "f713f43fd18241209b320123555bdde1";  
-    const redirectUri = "https://gianluh.github.io/Jammming/login";  
+    const redirectUri = "https://gianluh.github.io/Jammming/";  
     const scope = "playlist-modify-public playlist-modify-private user-read-private user-read-email";
     const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
 
